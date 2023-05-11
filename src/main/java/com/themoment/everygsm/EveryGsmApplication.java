34d3e7e0298031ae8d30f2cfe32c42acfd25828c
Junho-06 <k34920032@gmail.com>
2023-05-11 21:01:56 +0900

    project setting

---
+++ src/main/java/com/themoment/everygsm/EveryGsmApplication.java
@@ -0,0 +1,13 @@
+package com.themoment.everygsm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryGsmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryGsmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/themoment/everygsm/EveryGsmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themoment.everygsm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryGsmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
