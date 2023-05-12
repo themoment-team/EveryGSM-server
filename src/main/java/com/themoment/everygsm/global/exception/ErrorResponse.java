fe5b725a0154586e43c18ef33d3a6d12b010ef4c
Junho-06 <k34920032@gmail.com>
2023-05-12 09:54:02 +0900

    add CustomeException handling

---
+++ src/main/java/com/themoment/everygsm/global/exception/ErrorResponse.java
@@ -0,0 +1,26 @@
+package com.themoment.everygsm.global.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+    private final String formatNow = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd / HH : mm : ss "));
+    private final String detailMessage;
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity(String detailMessage, HttpStatus httpStatus) {
+        return ResponseEntity
+                .status(httpStatus)
+                .body(ErrorResponse.builder()
+                        .detailMessage(detailMessage)
+                        .build()
+                );
+    }
+}
