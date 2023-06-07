84fe0e6de7852cb6cec3a39d19f0f8ca1fffacba
Junho-06 <k34920032@gmail.com>
2023-06-08 01:48:34 +0900

    update heart service log warning

--- src/main/java/com/themoment/everygsm/domain/heart/service/HeartService.java
+++ src/main/java/com/themoment/everygsm/domain/heart/service/HeartService.java
@@ -10,10 +10,12 @@ import com.themoment.everygsm.global.redis.Entity.LockObject;
 import com.themoment.everygsm.global.redis.repository.RedisLockRepository;
 import com.themoment.everygsm.global.util.UserUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class HeartService {
@@ -39,7 +41,7 @@ public class HeartService {
                         .orElseThrow(() -> new CustomException("좋아요 누를 프로젝를 찾지 못했습니다.", HttpStatus.NOT_FOUND));
 
                 if (heartRepository.existsHeartByUserAndProject(user, project)){
-                    throw new CustomException("이미 좋아요를 누른 프로젝트 입니다.", HttpStatus.BAD_REQUEST);
+                    log.warn("유저가 이미 좋아요를 누른 프로젝트 입니다");
                 }
                 Heart heart = Heart.builder()
                         .user(user)
@@ -73,7 +75,7 @@ public class HeartService {
                     .orElseThrow(() -> new CustomException("좋아요 누를 프로젝를 찾지 못했습니다.", HttpStatus.NOT_FOUND));
 
             if (!heartRepository.existsHeartByUserAndProject(user, project)) {
-                throw new CustomException("좋아요를 누르지 않은 프로젝트 입니다.", HttpStatus.BAD_REQUEST);
+                log.warn("유저가 좋아요를 누르지 않은 프로젝트 입니다");
             }
 
         try {
